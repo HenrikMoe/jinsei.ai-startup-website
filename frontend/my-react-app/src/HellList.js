18ce8b33983efe4b0b3ec204ce248bed8305e05e
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2025-06-08 17:09:32 -0700

    Update HellList.js

--- frontend/my-react-app/src/HellList.js
+++ frontend/my-react-app/src/HellList.js
@@ -10,6 +10,9 @@ const HellList = () => {
         <p>
               All souls are eternalized in heaven or hell. Hell has multiple levels. Every soul condemed to hell awaits complete public prosecution.  Souls in the Eternal Mutilation and Killing department are suspected of capital treason, conspiracy, and crimes against humanity. Souls in the Eternal Darkness department are suspected of being either indirecty involved in treasonous conspiracy or generally detract from divine heteroskedastic neural states of consciousness. 
         </p>
+        <p>
+    Names released later.
+        </p>
       </div>
 
       <div className="aboutJinsei">
@@ -17,15 +20,15 @@ const HellList = () => {
         <ul>
            <li>Every Matrix Homoskedastic Modeling Farmer (Physical Structure Generators, General FUD Beneficiaries, Physical Structure Detonators) </li>
 
-          <li>Alexandra Moe</li>
-          <li>Erik Moe</li>
+  {/* <li>Alexandra Moe</li>
+          <li>Erik Moe</li> */}
          <li>Sex Traffickers</li>
          <li>Rapists</li>
          <li>Unjust Murderers</li>
         <li>FBI Stolen Home Porn Viewers</li>
             <li>CIA Stolen Home Porn Viewers</li>
             <li>Blackrock Stolen Home Porn Viewers</li>
-          <li>Georgia Crowley's Father</li>
+{/*<li>Georgia Crowley's Father</li>*/}
           <li>NWO Predator Squad Members</li>
           <li>Rothschild Scientists</li>
          
@@ -33,14 +36,14 @@ const HellList = () => {
           <li>NWO Blackrock Network Members</li>
           <li>NWO FBI Network Members</li>
           <li>NWO CIA Intellisgence Officers</li>
-          <li>David Muscatel</li>
+{/*<li>David Muscatel</li>
           <li>Ryan Burrow</li>
           <li>Hillary Clinton</li>
-          <li>George H.W. Bush</li>
+          <li>George H.W. Bush</li>*/}
           <li>NWO NGO Network Members</li>
           <li>Khazarian Network Members</li>
           <li>NWO Network Members</li>
-          <li>Volodimir Zelensky</li>
+{/*<li>Volodimir Zelensky</li>*/}
           <li>NWO Congress Members</li>
           <li>NWO Lobbyists </li>
          
@@ -50,27 +53,27 @@ const HellList = () => {
           <li>Grey Fagatron Network Members</li>
           <li>MS-13 Network Members</li>
           <li>Tren De Agua Network Members</li>
-          <li>L.R. Fox</li>
+{/*<li>L.R. Fox</li>
          <li>Suge Knight</li>
          <li>Yo Gotti</li>
          <li>Pope Francis IV</li>
         <li>Snoop Dogg</li>
         <li>Daz Dillinger</li>
         <li>Kurupt</li>
-        <li>Young Thug</li>
+        <li>Young Thug</li>*/}
         <li>Moon U Network Members</li>
-          <li>John Miller</li>
+         {/* <li>John Miller</li>
          <li>Luke Miller's Grandfather</li>
          <li>Jeffrey Epstein</li>
-         <li>Jay Z</li>
+         <li>Jay Z</li>*/}
          <li>Epstein Client List</li>
          <li>NWO Satanic Vatican Network Members</li>
-         <li>Chase Covel</li>
+         {/*<li>Chase Covel</li>
          
-         <li>Ryan From NWO</li>
+         <li>Ryan From NWO</li>*/}
          <li>Child Predators</li>
-         <li>Bill Gates</li>
-         <li>Oprah Winfrey</li>
+         {/*<li>Bill Gates</li>
+         <li>Oprah Winfrey</li>*/}
          <li>Pedophiles</li>
           <li>Child Molesters</li>
              <li>Child Rapists</li>
@@ -83,7 +86,7 @@ const HellList = () => {
             <li>Unconscious Homoskedastic Modeling Minded People </li>
           <li>Generally Predatory Gay People</li>
          <li>Generally Predatory Straight People</li>
-         <li>Henrik Moe II</li>
+         {/*<li>Henrik Moe II</li>
          <li>Luke Miller</li>
          <li>Fernando Hernandez</li>
            <li>Gretchyn Mooney</li>
@@ -106,7 +109,7 @@ const HellList = () => {
           <li>Vincent Bugni</li>
           <li>Georiga Crowley</li>
           <li>Claire Desmith </li>
-                  
+                  */}
 
         </ul>
 
