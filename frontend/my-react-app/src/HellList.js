0fa23e0f916bdc5751998f3b4f3ff51d4fd54745
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2025-06-14 21:03:19 -0700

    Update HellList.js

--- frontend/my-react-app/src/HellList.js
+++ frontend/my-react-app/src/HellList.js
@@ -8,114 +8,56 @@ const HellList = () => {
 
       <div className="aboutJinsei">
         <p>
-              All souls are eternalized in heaven or hell. Hell has multiple levels. Every soul condemed to hell awaits complete public prosecution.  Souls in the Eternal Mutilation and Killing department are suspected of capital treason, conspiracy, and crimes against humanity. Souls in the Eternal Darkness department are suspected of being either indirecty involved in treasonous conspiracy or generally detract from divine heteroskedastic neural states of consciousness. 
+          All souls are eternalized in heaven or hell. Hell has multiple levels. Every soul condemned to hell awaits complete public prosecution. Souls in the Eternal Mutilation and Killing department are suspected of capital treason, conspiracy, and crimes against humanity. Souls in the Eternal Darkness department are suspected of being either indirectly involved in treasonous conspiracy or generally detract from divine heteroskedastic neural states of consciousness.
         </p>
-        <p>
-    Names released later.
-        </p>
+        <p>Names released later.</p>
       </div>
 
       <div className="aboutJinsei">
         <h2>Suspected Souls Subject To Eternal Mutilation and Killing</h2>
         <ul>
-           <li>Every Matrix Homoskedastic Modeling Farmer (Physical Structure Generators, General FUD Beneficiaries, Physical Structure Detonators) </li>
-
-  {/* <li>Alexandra Moe</li>
-          <li>Erik Moe</li> */}
-         <li>Sex Traffickers</li>
-         <li>Rapists</li>
-         <li>Unjust Murderers</li>
-        <li>FBI Stolen Home Porn Viewers</li>
-            <li>CIA Stolen Home Porn Viewers</li>
-            <li>Blackrock Stolen Home Porn Viewers</li>
-{/*<li>Georgia Crowley's Father</li>*/}
-          <li>NWO Predator Squad Members</li>
-          <li>Rothschild Scientists</li>
-         
-          <li>NWO Northrup Gruman Operators</li>
-          <li>NWO Blackrock Network Members</li>
-          <li>NWO FBI Network Members</li>
-          <li>NWO CIA Intellisgence Officers</li>
-{/*<li>David Muscatel</li>
-          <li>Ryan Burrow</li>
-          <li>Hillary Clinton</li>
-          <li>George H.W. Bush</li>*/}
-          <li>NWO NGO Network Members</li>
-          <li>Khazarian Network Members</li>
-          <li>NWO Network Members</li>
-{/*<li>Volodimir Zelensky</li>*/}
-          <li>NWO Congress Members</li>
-          <li>NWO Lobbyists </li>
-         
-          <li>NWO Judicial Members</li>
-          <li>NWO Mason Members</li>
-          <li>NWO Lizard Human Hybrids</li>
-          <li>Grey Fagatron Network Members</li>
-          <li>MS-13 Network Members</li>
-          <li>Tren De Agua Network Members</li>
-{/*<li>L.R. Fox</li>
-         <li>Suge Knight</li>
-         <li>Yo Gotti</li>
-         <li>Pope Francis IV</li>
-        <li>Snoop Dogg</li>
-        <li>Daz Dillinger</li>
-        <li>Kurupt</li>
-        <li>Young Thug</li>*/}
-        <li>Moon U Network Members</li>
-         {/* <li>John Miller</li>
-         <li>Luke Miller's Grandfather</li>
-         <li>Jeffrey Epstein</li>
-         <li>Jay Z</li>*/}
-         <li>Epstein Client List</li>
-         <li>NWO Satanic Vatican Network Members</li>
-         {/*<li>Chase Covel</li>
-         
-         <li>Ryan From NWO</li>*/}
-         <li>Child Predators</li>
-         {/*<li>Bill Gates</li>
-         <li>Oprah Winfrey</li>*/}
-         <li>Pedophiles</li>
+          <li>Blackrock Network Stolen Home Porn Viewers</li>
           <li>Child Molesters</li>
-             <li>Child Rapists</li>
+          <li>Child Predators</li>
+          <li>Child Rapists</li>
+          <li>CIA Network Stolen Home Porn Viewers</li>
+          <li>Epstein Client List</li>
+          <li>Every Matrix Homoskedastic Modeling Farmer (Physical Structure Generators, General FUD Beneficiaries, Physical Structure Detonators)</li>
+          <li>FBI Network Stolen Home Porn Viewers</li>
+          <li>Grey Fagatron Network Members</li>
+          <li>Khazarian Network Members</li>
+          <li>MS-13 Network Members</li>
+          <li>Moon U Network Members</li>
+          <li>NWO Blackrock Network Members</li>
+          <li>NWO CIA Intelligence Officers</li>
+          <li>NWO Congress Members</li>
+          <li>NWO FBI Network Members</li>
+          <li>NWO Judicial Members</li>
+          <li>NWO Lizard Human Hybrids</li>
+          <li>NWO Lobbyists</li>
+          <li>NWO Mason Members</li>
+          <li>NWO Network Members</li>
+          <li>NWO NGO Network Members</li>
+          <li>NWO Northrup Gruman Operators</li>
+          <li>NWO Predator Squad Members</li>
+          <li>NWO Satanic Vatican Network Members</li>
+          <li>Pedophiles</li>
+          <li>Rapists</li>
+          <li>Rothschild Scientists</li>
+          <li>Sex Traffickers</li>
+          <li>Tren De Agua Network Members</li>
+          <li>Unjust Murderers</li>
         </ul>
       </div>
 
       <div className="aboutJinsei">
         <h2>Suspected Souls Subject to Eternity in Darkness</h2>
         <ul>
-            <li>Unconscious Homoskedastic Modeling Minded People </li>
           <li>Generally Predatory Gay People</li>
-         <li>Generally Predatory Straight People</li>
-         {/*<li>Henrik Moe II</li>
-         <li>Luke Miller</li>
-         <li>Fernando Hernandez</li>
-           <li>Gretchyn Mooney</li>
-         <li>Emmett Abdian </li>
-        <li>Max Chacon</li>
-         <li>Brendan Mooney</li>
-         <li>Michael Mooney</li>
-         <li>Roman Schneider </li>
-         <li>Israel Dedina</li>
-         <li>David Austin's Friend</li>
-        <li>Tomas Atkinson</li>
-          <li>Kal Kapur</li>
-          <li>Noah Schwartz</li>
-          <li>Stefano Domincalli</li>
-          <li>Jacob Mannisto</li>
-          <li>Jacob Jung</li>
-          <li>Gilles Kirshner</li>
-          <li>Taylor Garvey</li>
-          <li>Brody Taylor's Friend</li>
-          <li>Vincent Bugni</li>
-          <li>Georiga Crowley</li>
-          <li>Claire Desmith </li>
-                  */}
-
+          <li>Generally Predatory Straight People</li>
+          <li>Unconscious Homoskedastic Modeling Minded People</li>
         </ul>
-
       </div>
-
-     
 
       <div className="aboutJinsei69">
         <p>
